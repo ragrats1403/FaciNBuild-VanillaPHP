4e0021eead4d81d814861022a50d0ae98119e760
Masahiro Kikuchi <accelstudio.gt@gmail.com>
2023-02-27 23:10:33 +0800

    btn adjustment/change table bg

--- php/admin/accounts/admin_account.php
+++ php/admin/accounts/admin_account.php
@@ -110,16 +110,18 @@
                 <div class="container">
 
                     <div class="row">
-                        <div class="col-md-2"></div>
-                        <div class="col-md-9">
+                        <div class="col-md-2" style="width: 19%;"></div>
+                        <div class="col-sm-9" style="width: 75%; background-color: #FFF; padding-left:50px; padding-right:50px;">
                             <!-- padding-left:50px; padding-top:50px; padding-right:50px;-->
-                            <button type="button" class="btn btn-primary" style="margin-bottom:40px;" data-bs-toggle="modal" data-bs-target="#addUserModal">Add New Account</button>
+                            <div class="col-sm-12 d-flex justify-content-start pt-3">
+                            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addUserModal">Add New Account</button>
                         </div>
+                     </div>
                     </div>
                     
                     <div class="row">
-                        <div class="col-md-2"></div>
-                        <div class="col-md-9">
+                    <div class="col-md-2" style="width: 19%;"></div>
+                        <div class="col-md-9" style="width: 75%; background-color: #FFF; padding-left:50px; padding-right:50px; padding-bottom:50px; ">
                             <!-- padding-left:50px; padding-right:50px; padding-bottom:50px;-->
                             <table id="datatable" class="table">
                                 <thead>
