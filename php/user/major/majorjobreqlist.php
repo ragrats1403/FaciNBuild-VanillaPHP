7f6ed76f647c26e0b5a6907e51d37162618b11d8
P1XELATED <shirfredtacdoro@gmail.com>
2023-03-15 22:21:35 +0800

    remove Sectiondropdown

--- php/user/major/majorjobreqlist.php
+++ php/user/major/majorjobreqlist.php
@@ -353,16 +353,6 @@
                                     <textarea placeholder="Purpose" class="form-control" rows="2" id="majorjrpurp"></textarea>
                                 </div>
                             </div>
-                            <div class="row">
-                                <div class="col-md-12">
-                                    <label class="fw-bold" style="padding-bottom:5px;" for="date">Remarks:</label>
-                                    <select class="" style="width: 150px; Border: none;" name="cars" id="cars">
-                                        <option value="volvo">Outsource</option>
-                                        <option value="saab">Bill of materials</option>
-                                    </select>
-                                </div>
-                            </div>
-
                             <!-- Form Controls End-->
                         </div>
                         <div class="modal-footer justify-content-md-center">
