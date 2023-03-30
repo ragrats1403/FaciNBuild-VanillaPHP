eb12025c1164f83af16804d778513e9c69794ec3
Rivers Dela Serna <91451730+Daredev1l100@users.noreply.github.com>
2023-03-31 00:47:02 +0800

    PCO approve and decline process added

---
+++ php/pco/major/update_user.php
@@ -0,0 +1,26 @@
+<?php include('../../connection/connection.php');
+
+$jobreqno = $_POST['jobreqno'];
+$requino = $_POST['requino'];
+$department = $_POST['department'];
+$section = $_POST['section'];
+$quantity = $_POST['quantity'];
+$item = $_POST['item'];
+$description = $_POST['description'];
+$purpose = $_POST['purpose'];
+$outsource = $_POST['outsource'];
+
+
+$sql = "UPDATE `majoreq` SET `requino`='$requino', `department`= '$department', `section`= '$section' , `quantity`='$quantity',`item` = '$item' ,`description` = '$description' ,`purpose` = '$purpose' ,`outsource` = '$outsource' WHERE jobreqno = '$jobreqno'";
+$query = mysqli_query($con, $sql);
+if ($query == true) {
+    $data = array(
+        'status' => 'success',
+    );
+    echo json_encode($data);
+} else {
+    $data = array(
+        'status' => 'failed',
+    );
+    echo json_encode($data);
+}
