0baa52b490bd7a7d7aded317a450e6cf447eb6e4
P1XELATED <shirfredtacdoro@gmail.com>
2023-03-31 01:09:24 +0800

    RemovingFunctionstoSidebar

--- php/cad/major/majorjobreqlist.php
+++ php/cad/major/majorjobreqlist.php
@@ -33,7 +33,7 @@
         <span class='bx bxs-bell'></i>
         <span class="icon-button__badge"></span>
        </div>
-        <p>Hello, Administrator</p>
+        <p>Hello, CAD</p>
       <nav class="gnav">
         </nav>
     </div>
@@ -56,12 +56,6 @@
                     </a>
             </li>
             <li>
-                <a href="../../../../php/admin/accounts/admin_account.php">
-                    <i class='bx bx-user'></i>
-                    <span class="link_name">Account</span>
-                </a>
-            </li>
-            <li>
                 <div class="dropdown">
                     <i class='bx bx-clipboard' style="margin-left:17px;" ></i>
                     <span class="jobrequestdr btn dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -72,12 +66,6 @@
                         <a class="dropdown-item" href="../../../../php/admin/jobrequest/major/majorjobreqlist.php">Major Job Request</a>
                     </ul>
                 </div>
-            </li>
-            <li>
-                <a href="../../../../php/admin/equipments/adminequipment.php">
-                    <i class='bx bx-wrench'></i>
-                    <span class="link_name">Equipment</span>
-                </a>
             </li>
             <li>
                 <a href="../../../../php/admin/reservations/adminreservations.php">
@@ -91,8 +79,8 @@
                     <div class="profile_details">
                     <img src="../../../../images/ico/profileicon.png" alt="" style = "height: 45px; width:45px; object-fit:cover; border-radius:12px;" />
                         <div class="name_role">
-                            <div class="name">Admin</div>
-                            <div class="role">System Administrator</div>
+                            <div class="name">CAD</div>
+                            <div class="role">CAD</div>
                         </div>
                     </div>
                     <a href="../../../../logout.php">
