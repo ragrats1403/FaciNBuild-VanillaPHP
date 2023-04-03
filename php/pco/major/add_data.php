ffe094ba6fe6aa5f5f72b09a24ff710e15500ffa
Rivers Dela Serna <91451730+Daredev1l100@users.noreply.github.com>
2023-04-03 20:06:32 +0800

    updated backend for PCO

--- php/pco/major/add_data.php
+++ php/pco/major/add_data.php
@@ -5,12 +5,10 @@ $department = $_POST['department'];
 $date = $_POST['date'];
 $quantity = $_POST['quantity'];
 $item = $_POST['item'];
-$section = $_POST['section'];
 $description = $_POST['description'];
 $purpose = $_POST['purpose'];
-$outsource = $_POST['outsource'];
 
-$sql = "INSERT INTO `majoreq` (`requino`,`department`,`date`,`quantity`,`item`,`section`,`description`,`purpose`,`outsource`,`status`,`bdstatus`,`cadstatus`,`pcostatus`) VALUES ('$requino','$department','$date','$quantity','$item','$section','$description','$purpose','$outsource','Pending','Pending','Pending','Pending')";
+$sql = "INSERT INTO `majoreq` (`requino`,`department`,`date`,`quantity`,`item`,`description`,`purpose`,`status`,`bdstatus`,`cadstatus`,`pcostatus`) VALUES ('$requino','$department','$date','$quantity','$item','$description','$purpose','Pending','Pending','Pending','Pending')";
 $query = mysqli_query($con, $sql);
 if ($query == true) {
     $data = array(
