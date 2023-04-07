a469a0aa9726ddd5514a06f6a20551cdca80ae6e
Ragrats- <91531302+ragrats1403@users.noreply.github.com>
2023-04-07 22:45:03 +0800

    80% user minor job request done + fix

---
+++ php/user/minor/functions/add_data.php
@@ -0,0 +1,36 @@
+<?php include('../../../connection/connection.php');
+
+
+$department = $_POST['department'];
+$date = $_POST['date'];
+$quantity = $_POST['quantity'];
+$itemname = $_POST['itemname'];
+$description = $_POST['description'];
+$purpose = $_POST['purpose'];
+
+/*$renderedby = $_POST['renderedby'];
+$daterendered = $_POST['daterendered'];
+$confirmedby = $_POST['confirmedby'];
+$dateconfirmed = $_POST['dateconfirmed'];*/
+
+
+
+$sql = "INSERT INTO `minorjreq` (`department`,`datesubmitted`, `quantity`, `item`,`item_desc`,`purpose`, `bdstatus`,`status`) VALUES ('$department','$date','$quantity','$itemname','$description',' $purpose','Pending', 'Pending')";
+$query = mysqli_query($con, $sql);
+if ($query == true) {
+    $data = array(
+        'status' => 'success',
+    );
+    echo json_encode($data);
+} else {
+
+    $data = array(
+        'status' => 'failed',
+    );
+    echo json_encode($data);
+}
+
+
+
+
+?>
--- php/user/reservation/userreservation.php
+++ php/user/reservation/userreservation.php
@@ -226,7 +226,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script>
         //table display start
-        var dpt = "<?php echo mb_strimwidth($_SESSION['department'], 0, 20, '…');?>";
+        var dpt = "<?php echo $_SESSION['department'];?>";
             $("#datatable").DataTable({
             serverSide: true,
             processing: true,
