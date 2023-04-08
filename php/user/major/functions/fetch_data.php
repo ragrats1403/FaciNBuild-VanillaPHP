799a2a9fe6be69a91b92fa86f4fa06757ac2c07d
Rivers Dela Serna <91451730+Daredev1l100@users.noreply.github.com>
2023-04-08 17:31:11 +0800

    remove print button user

--- php/user/major/functions/fetch_data.php
+++ php/user/major/functions/fetch_data.php
@@ -44,8 +44,7 @@ while ($row = mysqli_fetch_assoc($run_query)) {
     $subarray[] = $row['department'];
     $subarray[] = $row['quantity']; 
     $subarray[] = $row['status']; 
-    $subarray[] = '<a href= "javascript:void();" data-id="' . $row['jobreqno'] . '" class ="btn btn-sm btn-info editBtn">More Info</a> 
-    <a href= "javascript:void();" onclick="printDiv()" data-id="' . $row['jobreqno'] . '" class ="btn btn-sm btn-secondary btnprint">Print</a>';
+    $subarray[] = '<a href= "javascript:void();" data-id="' . $row['jobreqno'] . '" class ="btn btn-sm btn-info editBtn">More Info</a>';
     $data[] = $subarray;
 }
 
