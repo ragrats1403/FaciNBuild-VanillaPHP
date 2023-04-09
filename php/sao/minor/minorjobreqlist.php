c3edc2506d468a3721cfc10baafcc5e465bc9ead
Rivers Dela Serna <91451730+Daredev1l100@users.noreply.github.com>
2023-04-10 03:53:33 +0800

    added column table

--- php/sao/minor/minorjobreqlist.php
+++ php/sao/minor/minorjobreqlist.php
@@ -186,6 +186,7 @@
                                     <th>ID</th>
                                     <th>Department</th>
                                     <th>Date</th>
+                                    <th>Section</th>
                                     <th>Status</th>
                                     <th>Options</th>
                                 </thead>
