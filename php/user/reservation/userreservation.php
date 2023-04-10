6a0e24c277c7d59e776a0898b6d738cd33f8fea1
Ragrats- <91531302+ragrats1403@users.noreply.github.com>
2023-04-10 20:31:38 +0800

    Yes

--- php/user/reservation/userreservation.php
+++ php/user/reservation/userreservation.php
@@ -42,14 +42,6 @@
             const notificationDropdown = document.getElementById("notification-dropdown");
             const notificationBadge = notificationDropdown.querySelector(".icon-button__badge");
 
-<<<<<<< Updated upstream
-            // Get the notification list element
-            const notificationList = document.querySelector(".notification-list");
-            notificationList.style.height = "300px"; // Set a fixed height for the notification
-            notificationList.style.overflowY = "auto"; // Enable vertical scrolling
-            notificationList.style.width = "500px";
-            notificationList.style.position = "relative";
-=======
 // Get the notification list element
 const notificationList = document.querySelector(".notification-list");
 notificationList.style.height = "300px"; // Set a fixed height for the notification
@@ -71,7 +63,6 @@ function fetchNotifications() {
       var len = notifications.length;
       // Update the badge count
       notificationBadge.innerText = notifications.length;
->>>>>>> Stashed changes
 
             // Fetch the notifications and update the badge and list
             function fetchNotifications() {
@@ -126,7 +117,9 @@ function fetchNotifications() {
                     }
                 });
             }
-
+        }
+    });
+}
             document.addEventListener("DOMContentLoaded", function() {
                 fetchNotifications();
                 setInterval(fetchNotifications, 5000);
