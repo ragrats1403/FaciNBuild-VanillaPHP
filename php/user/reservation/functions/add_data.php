554d42435c12766a7a7b32964d970e7cb48b4063
Ragrats- <91531302+ragrats1403@users.noreply.github.com>
2023-04-10 16:53:20 +0800

    Dynamic Notification for users side

--- php/user/reservation/functions/add_data.php
+++ php/user/reservation/functions/add_data.php
@@ -29,18 +29,26 @@ echo "Error: ". $psql . "<br>" . mysqli_error($con);
 }*/
 
 
-//conflict checking section start
+//notification message construct start
+$datestr = date('Y-m-d H:i:s');
+$date = new DateTime($datestr);
+$formatted_date = $date->format('F d, Y g:iA');
+
+$actualdatestr = date($actualdate);
+$adate = new DateTime($actualdatestr);
+$aformatted_date = $adate->format('F d, Y');
+
+$message = "You have submitted your facility reservation to use on \n".$aformatted_date."";
 
 
-
-
-
-//conflict checking section end
+//notification message construct end
 
 
 $sql = "INSERT INTO `reservation` (`eventname`, `facility`, `requestingparty`, `purposeofactivity`, `datefiled`, `actualdateofuse`, `timestart`, `timeend`, `participants`, `stageperformers`, `adviser`, `chairperson`, `status`, `fdstatus`, `saostatus`) VALUES ('$eventname','$faci','$reqparty',' $purpose', '$datefiled', '$actualdate', '$timein', '$timeout', '$numparticipants', '$stageperf', '$adviser', '$chairman', 'Pending', 'Pending', 'Pending')";
 $query = mysqli_query($con, $sql);
 if ($query == true) {
+    $sqlnotif = "INSERT INTO `notif_data` (`message`, `department`, `created_at`, `is_read`) VALUES ('$message', '$reqparty', '$datestr','0')";
+    $notifquery = mysqli_query($con, $sqlnotif);
     $data = array(
         'status' => 'success',
     );
