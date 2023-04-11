94f021d8e9cec4d06ead4ae3d7c690a05c93f54e
Ragrats- <91531302+ragrats1403@users.noreply.github.com>
2023-04-11 17:02:33 +0800

    pco,user,sao,cad,buildingdept Minor an Major job request add_data.php backend DONE

--- php/cad/majoruser/functions/add_data.php
+++ php/cad/majoruser/functions/add_data.php
@@ -7,9 +7,36 @@ $item = $_POST['item'];
 $description = $_POST['description'];
 $purpose = $_POST['purpose'];
 
+date_default_timezone_set('Asia/Manila');
+
+// Get the current datetime in the server timezone
+$date = new DateTime('now', new DateTimeZone(date_default_timezone_get()));
+
+// Set the timezone to your local timezone
+$date->setTimezone(new DateTimeZone(date_default_timezone_get()));
+
+// Format the date in your local timezone
+$formatted_date = $date->format('Y-m-d H:i:s');
+
+$message = "You have submitted a Major Job Request and is now Pending for approval.";
+$facilitiesDeptmesg = "".$department." submitted a Major Job Request and is waiting for Approval\nCheck them in Manage Job Requests1";
+$saoDeptmesg = "".$department." submitted a Major Job Request and is waiting for Approval\nCheck them in Manage Job Requests!";
+$adminDeptmesg = "".$department." submitted a Major Job Request and is waiting for Approval\nCheck them in Manage Job Requests!";
+$caddeptmsg = "".$department." submitted a Major Job Request and is waiting for Approval\nCheck them in Manage Job Requests!";
+
 $sql = "INSERT INTO `majoreq` (`department`,`date`,`quantity`,`item`,`description`,`purpose`,`status`,`bdstatus`,`cadstatus`,`pcostatus`) VALUES ('$department','$date','$quantity','$item','$description','$purpose','Pending','Pending','Pending','Pending')";
 $query = mysqli_query($con, $sql);
 if ($query == true) {
+    $sqlnotif = "INSERT INTO `notif_data` (`message`, `department`, `created_at`, `is_read`) VALUES ('$message', '$department', '$formatted_date','0')";
+    $notifquery = mysqli_query($con, $sqlnotif);
+    $sqlnotiffaci = "INSERT INTO `notif_data` (`message`, `department`, `created_at`, `is_read`) VALUES ('$facilitiesDeptmesg', 'Building Department', '$formatted_date','0')";
+    $facinotifquery = mysqli_query($con, $sqlnotiffaci);
+    $sqlnotifsao = "INSERT INTO `notif_data` (`message`, `department`, `created_at`, `is_read`) VALUES ('$saoDeptmesg', 'Property Custodians Office', '$formatted_date','0')";
+    $saonotifquery = mysqli_query($con, $sqlnotifsao);
+    $sqlnotifadmin = "INSERT INTO `notif_data` (`message`, `department`, `created_at`, `is_read`) VALUES ('$adminDeptmesg', 'Administrator', '$formatted_date','0')";
+    $adminnotifquery = mysqli_query($con, $sqlnotifadmin);
+    $sqlnotifcad = "INSERT INTO `notif_data` (`message`, `department`, `created_at`, `is_read`) VALUES ('$adminDeptmesg', 'Campus Academic Directors Office', '$formatted_date','0')";
+    $cadnotifquery = mysqli_query($con, $sqlnotifcad);
     $data = array(
         'status' => 'success',
     );
--- php/sao/minor/functions/add_data.php
+++ php/sao/minor/functions/add_data.php
@@ -8,16 +8,32 @@ $itemname = $_POST['itemname'];
 $description = $_POST['description'];
 $purpose = $_POST['purpose'];
 
-/*$renderedby = $_POST['renderedby'];
-$daterendered = $_POST['daterendered'];
-$confirmedby = $_POST['confirmedby'];
-$dateconfirmed = $_POST['dateconfirmed'];*/
+date_default_timezone_set('Asia/Manila');
+
+// Get the current datetime in the server timezone
+$date = new DateTime('now', new DateTimeZone(date_default_timezone_get()));
+
+// Set the timezone to your local timezone
+$date->setTimezone(new DateTimeZone(date_default_timezone_get()));
+
+// Format the date in your local timezone
+$formatted_date = $date->format('Y-m-d H:i:s');
+
+$message = "You have submitted a Minor Job Request and is now Pending for approval.";
+$facilitiesDeptmesg = "".$department." submitted a Minor Job Request and is waiting for Approval\nCheck them in Manage Job Requests1";
+$adminDeptmesg = "".$department." submitted a Minor Job Request and is waiting for Approval\nCheck them in Manage Job Requests!";
 
 
-
-$sql = "INSERT INTO `minorjreq` (`department`,`datesubmitted`, `quantity`, `item`,`item_desc`,`purpose`,`bdstatus`,`status`) VALUES ('$department','$date','$quantity','$itemname','$description',' $purpose','Pending', 'Pending')";
+$sql = "INSERT INTO `minorjreq` (`department`,`datesubmitted`, `quantity`, `item`,`item_desc`,`purpose`, `bdstatus`,`status`) VALUES ('$department','$date','$quantity','$itemname','$description',' $purpose','Pending', 'Pending')";
 $query = mysqli_query($con, $sql);
 if ($query == true) {
+    $sqlnotif = "INSERT INTO `notif_data` (`message`, `department`, `created_at`, `is_read`) VALUES ('$message', '$department', '$formatted_date','0')";
+    $notifquery = mysqli_query($con, $sqlnotif);
+    $sqlnotiffaci = "INSERT INTO `notif_data` (`message`, `department`, `created_at`, `is_read`) VALUES ('$facilitiesDeptmesg', 'Building Department', '$formatted_date','0')";
+    $facinotifquery = mysqli_query($con, $sqlnotiffaci);
+    $sqlnotifadmin = "INSERT INTO `notif_data` (`message`, `department`, `created_at`, `is_read`) VALUES ('$adminDeptmesg', 'Administrator', '$formatted_date','0')";
+    $adminnotifquery = mysqli_query($con, $sqlnotifadmin);
+
     $data = array(
         'status' => 'success',
     );
