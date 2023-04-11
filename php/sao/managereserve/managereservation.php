14ef1b4341b9c344904e38bbb2c69fe4d2b1a821
Masahiro Kikuchi <accelstudio.gt@gmail.com>
2023-04-11 16:37:56 +0800

    sao manage reservation upd

--- php/sao/managereserve/managereservation.php
+++ php/sao/managereserve/managereservation.php
@@ -137,47 +137,42 @@
 </header>
 
 <body onload="fetchNotifications();">
-    <div class="sidebar">
+<div class="sidebar">
         <div class="logo_content">
             <div class="logo">
-                <img src="../../../images/Black_logo.png" />
+                <img src="../../../../images/Brown_logo_faci.png" />
             </div>
         </div>
-
         <div class="navdiv">
             <ul class="nav_list">
-                <li>
-                    <a href="../../../php/admin/reservations/admincalendar.php">
-                        <i class='bx bx-calendar'></i>
-                        <span class="link_name">Calendar of Activities</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="../../../php/admin/accounts/admin_account.php">
-                        <i class='bx bx-user'></i>
-                        <span class="link_name">Account</span>
-                    </a>
-                </li>
-                <li>
-                    <div class="dropdown">
-                        <i class='bx bx-clipboard' style="margin-left:17px;"></i>
-                        <span class="jobrequestdr btn dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Create/Manage Request
-                        </span>
-                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                            <a class="dropdown-item" href="../../../php/admin/jobrequest/minor/minorjobreqlist.php">Minor Job Request</a>
-                            <a class="dropdown-item" href="../../../php/admin/jobrequest/major/majorjobreqlist.php">Major Job Request</a>
-                            <a class="dropdown-item" href="../../../php/admin/reservations/adminreservations.php">Reservations</a>  
-                        </ul>
-                    </div>
-                </li>
-                <li>
-                    <a href="../../../php/admin/equipments/adminequipment.php">
-                        <i class='bx bx-wrench'></i>
-                        <span class="link_name">Facilities Equipment</span>
-                    </a>
-                </li>
-            </ul>
+                <ul class="nav_list">
+                    <li>
+                        <a href="../../../php/sao/reservation/saocalendar.php">
+                            <i class='bx bx-calendar'></i>
+                            <span class="link_name">Calendar of Activities</span>
+                        </a>
+                    </li>
+                    <li>
+                        <div class="dropdown">
+                            <i class='bx bx-notepad' style="margin-left:17px;"></i>
+                            <span class="jobrequestdr btn dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Manage Request
+                            </span>
+                            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                <a class="dropdown-item" href="../../../php/sao/managereserve/managereservation.php">Reservation</a>
+                            </ul>
+                        </div>
+                        <div class="dropdown">
+                            <i class='bx bx-clipboard' style="margin-left:17px;"></i>
+                            <span class="jobrequestdr btn dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                View/Create Request
+                            </span>
+                            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                <a class="dropdown-item" href="../../../php/sao/major/majorjobreqlist.php">Major Request</a>
+                                <a class="dropdown-item" href="../../../php/sao/minor/minorjobreqlist.php">Minor Request</a>
+                                <a class="dropdown-item" href="../../../php/sao/reservation/saoreservation.php">Reservation</a>
+                            </ul>
+                </ul>
 
             <div class="profile_content">
                 <div class="profile">
@@ -185,7 +180,7 @@
                         <img src="../../../images/ico/profileicon.png" alt="" style="height: 45px; width:45px; object-fit:cover; border-radius:12px;" />
                         <div class="name_role">
                         <div class="name"><?php echo mb_strimwidth($_SESSION['department'], 0, 20, '…');?></div>
-                            <div class="role">System Administrator</div>
+                            <div class="role">SAO</div>
                         </div>
                     </div>
                     <a href="../../../logout.php">
