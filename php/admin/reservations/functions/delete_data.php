83b0cf3153a819e44ec4645d6ce31f91b9472312
Ragrats- <91531302+ragrats1403@users.noreply.github.com>
2023-04-13 23:45:50 +0800

    Update delete_data.php

--- php/admin/reservations/functions/delete_data.php
+++ php/admin/reservations/functions/delete_data.php
@@ -2,7 +2,7 @@
 
 $id = $_POST['id'];
 
-$sql = "DELETE FROM `reservation` WHERE reservationid ='$id' and `status` = 'Pending'";
+$sql = "DELETE FROM `reservation` WHERE reservationid ='$id' and `status` = 'Pending' OR `status` = 'Declined'";
 $query = mysqli_query($con, $sql);
 if ($query == true) 
 {
