f4a9f6504b42478e255e41aa5e11010736dba07c
Masahiro Kikuchi <accelstudio.gt@gmail.com>
2023-04-16 22:47:38 +0800

    Revert "generate report sidebar added (user)"
    This reverts commit 54a28e933a4ccdf6becf75026ea260210ecbc709.

--- php/user/major/majorjobreqlist.php
+++ php/user/major/majorjobreqlist.php
@@ -172,12 +172,6 @@
                     <span class="link_name">Reservation</span>
                 </a>
             </li>
-            <li>
-                    <a href="#">
-                        <i class='bx bx-printer'></i>
-                        <span class="link_name">Generate Report</span>
-                    </a>
-                </li>
         </ul>
         <div class="profile_content">
                 <div class="profile">
