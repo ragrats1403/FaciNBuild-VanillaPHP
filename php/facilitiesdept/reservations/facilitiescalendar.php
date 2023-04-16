10bf0267fb0899ea1b04e1aa136e75e738fc1dea
Masahiro Kikuchi <accelstudio.gt@gmail.com>
2023-04-16 22:30:12 +0800

    generate report sidebar added (facilities)

--- php/facilitiesdept/reservations/facilitiescalendar.php
+++ php/facilitiesdept/reservations/facilitiescalendar.php
@@ -183,6 +183,12 @@
                         </ul>
                     </div>
                 </li>
+                <li>
+                    <a href="#">
+                        <i class='bx bx-printer'></i>
+                        <span class="link_name">Generate Report</span>
+                    </a>
+                </li>
             </ul>
             <div class="profile_content">
                 <div class="profile">
