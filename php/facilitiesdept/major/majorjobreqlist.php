60aee4324fe2d1e06bb3ee50eebc65d7d0c14d29
Rivers Dela Serna <91451730+Daredev1l100@users.noreply.github.com>
2023-04-16 23:42:03 +0800

    added generate reports FRONEND FD

--- php/facilitiesdept/major/majorjobreqlist.php
+++ php/facilitiesdept/major/majorjobreqlist.php
@@ -182,6 +182,12 @@
                         </ul>
                     </div>
                 </li>
+                <li>
+                    <a href="../../../php/facilitiesdept/generatereports/generatereports.php">
+                        <i class='bx bx-food-menu'></i>
+                        <span class="link_name">Generate Report</span>
+                    </a>
+                </li>
             </ul>
             <div class="profile_content">
                 <div class="profile">
