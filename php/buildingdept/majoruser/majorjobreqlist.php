7c0f7bc298faa679c36e9b8cda126e476557567e
Masahiro Kikuchi <accelstudio.gt@gmail.com>
2023-04-16 22:35:19 +0800

    generate report sidebar added (building)

--- php/buildingdept/majoruser/majorjobreqlist.php
+++ php/buildingdept/majoruser/majorjobreqlist.php
@@ -179,6 +179,12 @@
                         </ul>
                     </div>
                 </li>
+                <li>
+                    <a href="#">
+                        <i class='bx bx-printer'></i>
+                        <span class="link_name">Generate Report</span>
+                    </a>
+                </li>
             </ul>
             <div class="profile_content">
                 <div class="profile">
