dae08440f35889641508b49cf65fcb905c77d077
Masahiro Kikuchi <accelstudio.gt@gmail.com>
2023-04-17 20:41:40 +0800

    authentication building

--- php/buildingdept/majoruser/majorjobreqlist.php
+++ php/buildingdept/majoruser/majorjobreqlist.php
@@ -1,3 +1,6 @@
+<?php
+require_once('../../authentication/anti_pagetrans.php');
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -31,9 +34,7 @@
                 <a class="dropdown-item py-1 px-2 text-center mark-as-read" href="#">Mark all as read</a>
             </div>
         </div>
-        <?php
-        session_start();
-        ?>
+
         <script>
             // Get the notification dropdown button and badge
             const notificationDropdown = document.getElementById("notification-dropdown");
@@ -192,7 +193,7 @@
                         <img src="../../../../images/ico/profileicon.png" alt="" style="height: 45px; width:45px; object-fit:cover; border-radius:12px;" />
                         <div class="name_role">
                             <div class="name"><?php echo mb_strimwidth($_SESSION['department'], 0, 20, '…'); ?></div>
-                            <div class="role">User</div>
+                            <div class="role">Building Department</div>
                         </div>
                     </div>
                     <a href="../../../../logout.php">
