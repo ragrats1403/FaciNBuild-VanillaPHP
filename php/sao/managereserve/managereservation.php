ddc8cfef128e05051986b9677933783258d723bd
Masahiro Kikuchi <accelstudio.gt@gmail.com>
2023-04-18 18:39:15 +0800

    table color added sao

--- php/sao/managereserve/managereservation.php
+++ php/sao/managereserve/managereservation.php
@@ -201,6 +201,7 @@ require_once('../../authentication/anti_pagetrans.php');
                 </div>
         </div>
     </div>
+    
     <div class="table1">
 
         <div class="container-fluid">
@@ -230,9 +231,43 @@ require_once('../../authentication/anti_pagetrans.php');
 <!-- Script Process Start-- DO NOT MOVE THIS Script tags!!-->
 <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
     <script type="text/javascript" src="https://cdn.datatables.net/v/bs5/jq-3.6.0/dt-1.13.1/datatables.min.js"></script>
-    <script type="text/javascript" src="functions/js/adminprocess.js?random=<?php echo uniqid(); ?>"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <!-- Modal Popup for More Info button-->
+    <script>
+        var dpt = "<?php echo $_SESSION['department'];?>";
+        $('#datatable').DataTable({
+    'serverSide': true,
+    'processing': true,
+    'paging': true,
+    'order': [],
+    'ajax': {
+        'url': 'functions/fetch_data.php',
+        'type': 'post',
+        'data': {
+            dpt: dpt,
+        },
+    },
+    'fnCreatedRow': function(nRow, aData, iDataIndex) {
+        $(nRow).attr('id', aData[0]);
+        if (aData[6] === 'Approved') {
+            $(nRow).css('background-color', '#a7d9ae');
+        }
+        if (aData[6] === 'Declined') {
+            $(nRow).css('background-color', '#e09b8d');
+        }
+        if (aData[6] === 'Pending') {
+            $(nRow).css('background-color', '#d9d2a7');
+        }
+    },
+    'columnDefs': [{
+        'targets': [0, 4],
+        'orderable': false,
+    }],
+    scrollY: 200,
+    scrollCollapse: true,
+    paging: false,
+});
+    </script>
     <div class="modal fade" id="test" aria-hidden="true">
     <div class="modal-dialog" style="max-width:1100px;">
             <div class="modal-content">
