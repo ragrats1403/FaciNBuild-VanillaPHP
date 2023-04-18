b6c9357f0fc8931cba64f9a9976122014c92c114
Rivers Dela Serna <91451730+Daredev1l100@users.noreply.github.com>
2023-04-18 20:20:28 +0800

    Revert "replaced dropdown button in all generate reports"
    This reverts commit b1c2214291ee945d4928818b53f3eaafd74f4353.

--- php/sao/generatereports/generatereports.php
+++ php/sao/generatereports/generatereports.php
@@ -227,9 +227,14 @@ require_once('../../authentication/anti_pagetrans.php');
                             </div>
                             <div class="col-md-2">
                                 <div class="dropdown">
-                                    <select class="form-control input-sm col-xs-1" name="sections" id="faci" onchange="dynamicEq()">
-                                        
-                                    </select>
+                                    <button class="btn btn-info dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                                        Dropdown button
+                                    </button>
+                                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                                        <li><a class="dropdown-item" href="#">Minor Job Request</a></li>
+                                        <li><a class="dropdown-item" href="#">Major Job Request</a></li>
+                                        <li><a class="dropdown-item" href="#">Reservation</a></li>
+                                    </ul>
                                 </div>
                             </div>
                         </div>
