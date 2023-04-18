92f403fe9727bcde28419ff830c4c4b23d209073
Masahiro Kikuchi <accelstudio.gt@gmail.com>
2023-04-18 19:05:30 +0800

    table color added sao

--- php/sao/managereserve/managereservation.php
+++ php/sao/managereserve/managereservation.php
@@ -231,6 +231,7 @@ require_once('../../authentication/anti_pagetrans.php');
 <!-- Script Process Start-- DO NOT MOVE THIS Script tags!!-->
 <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
     <script type="text/javascript" src="https://cdn.datatables.net/v/bs5/jq-3.6.0/dt-1.13.1/datatables.min.js"></script>
+    <script type="text/javascript" src="functions/js/adminprocess.js?random=<?php echo uniqid(); ?>"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <!-- Modal Popup for More Info button-->
     <script>
