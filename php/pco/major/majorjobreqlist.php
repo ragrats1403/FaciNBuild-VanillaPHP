1f71d4cd85e2b026493fe7d2d74ca43baa8d2f9a
Masahiro Kikuchi <accelstudio.gt@gmail.com>
2023-04-20 01:28:51 +0800

    delete generate report from sidebar

--- php/pco/major/majorjobreqlist.php
+++ php/pco/major/majorjobreqlist.php
@@ -177,12 +177,6 @@ require_once('../../authentication/anti_pagetrans.php');
                         </ul>
                     </div>
                 </li>
-                <li>
-                    <a href="../../../php/pco/generatereports/generatereports.php">
-                        <i class='bx bx-food-menu'></i>
-                        <span class="link_name">Generate Report</span>
-                    </a>
-                </li>
             </ul>
             <div class="profile_content">
                 <div class="profile">
