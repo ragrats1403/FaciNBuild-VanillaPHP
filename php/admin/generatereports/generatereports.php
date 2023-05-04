43e106a45e36d6a85a29197de3f248749b95ee7c
Rivers Dela Serna <91451730+Daredev1l100@users.noreply.github.com>
2023-05-04 08:54:09 +0800

    added banner generate reports admin

--- php/admin/generatereports/generatereports.php
+++ php/admin/generatereports/generatereports.php
@@ -344,14 +344,11 @@ require_once('../../authentication/anti_pagetrans.php');
                                 <button class="btn btn-info" onclick="printDiv()">Print</button>
                             </div>
 
-                            <div class="row" id="headerDiv" style="display: none;">
-                                <div class="col-md-1 float-start">
-                                    <img style="position: relative;" src='../../../../images/uclogo.png' alt='' width='75' height='50' />
-                                </div>   
-                                <div class="col-md-3">
-                                    <h1 class="fw-bold form-check-label" style="padding-left: 100px; position: relative;">Generate Reports </h1>
-                                </div>     
-                            </div>
+                            <div id="headerDiv" style="display: none; width: 100%;">
+                                <div class="col-auto">
+                                    <img src="../../../../images/ucbanner.jpg" class="img-fluid" alt="Responsive image">
+                                </div>
+                            </div>  
                             <div id="catch" style="display: none;">
                                 <label class="fw-bold">ㅤ</label>
                             </div>
