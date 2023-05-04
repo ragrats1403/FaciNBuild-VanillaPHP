ab3d0266aa6d19ca069cb3f683a71fa1e49f6503
Ragrats- <91531302+ragrats1403@users.noreply.github.com>
2023-05-05 00:51:25 +0800

    Fix divs for user minor job req

--- php/user/minor/minorjobreqlist.php
+++ php/user/minor/minorjobreqlist.php
@@ -600,7 +600,8 @@ $('#datatable').DataTable({
 
                                         if(num <= 5 && num >= 2)
                                         {
-                                            myFunctionPrompt(num);
+                                            var divid = num+"_";
+                                            myFunctionPrompt(divid);
                                             var newnum = num - 1;
                                             $('#numForms').val(newnum);
                                         }
@@ -619,7 +620,8 @@ $('#datatable').DataTable({
                                         {
                                             var newnum = parseInt(num) + 1;
                                             $('#numForms').val(newnum);
-                                            myFunctionPrompt(newnum);
+                                            var divid = newnum+"_";
+                                            myFunctionPrompt(divid);
                                         }
                                         else
                                         {
@@ -651,7 +653,7 @@ $('#datatable').DataTable({
                                 </div>
                             </div>
                             
-                            <div class = "2" style= "display:none;" id="2">
+                            <div class = "2" style= "display:none;" id="2_">
                             <hr class="solid">
                                     <div class="justify-content-center">                                 
                                         <div class="col-md-2" style="padding-bottom:10px">
@@ -668,7 +670,7 @@ $('#datatable').DataTable({
                                     </div>
                             <hr class="solid">
                             </div>
-                            <div class = "3" style= "display:none;" id="3">
+                            <div class = "3" style= "display:none;" id="3_">
                                     <div class="justify-content-center">                                 
                                         <div class="col-md-2" style="padding-bottom:10px">
                                             <label class="fw-bold" for="date">Quantity:</label>
@@ -683,7 +685,7 @@ $('#datatable').DataTable({
                                 </div>
                                 <hr class="solid">
                             </div>
-                            <div class = "4" style= "display:none;" id="4">
+                            <div class = "4" style= "display:none;" id="4_">
                                     <div class="justify-content-center">                                 
                                         <div class="col-md-2" style="padding-bottom:10px">
                                             <label class="fw-bold" for="date">Quantity:</label>
@@ -698,7 +700,7 @@ $('#datatable').DataTable({
                                 </div>
                                 <hr class="solid">
                             </div>
-                            <div class = "5" style= "display:none;" id="5">
+                            <div class = "5" style= "display:none;" id="5_">
                                     <div class="justify-content-center">                                 
                                         <div class="col-md-2" style="padding-bottom:10px">
                                             <label class="fw-bold" for="date">Quantity:</label>
